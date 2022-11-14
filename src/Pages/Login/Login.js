64f4481ed950150e502bcc68f978e51f3e27a500
Imtius-Ahammed <ahammedimtius098@gmail.com>
2022-11-14 11:08:44 +0600

    create env file

---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  const { register, handleSubmit } = useForm();
+  const handleLogin = data=>{
+  console.log(data)
+  }
+  return (
+    <div className="h-[800px] w-96 flex justify-center items-center">
+      <div className="w-96 p-7 ">
+        <h1 className="text-xl text-center"> Login</h1>
+        <form onSubmit={handleSubmit(handleLogin)}>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="text"
+              className="input input-bordered w-full max-w-xs"
+              {...register("email")}
+            />
+          </div>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              className="input input-bordered w-full max-w-xs"
+              {...register("password")}
+            />
+            <label className="label">
+              <span className="label-text">Forgot Password</span>
+            </label>
+          </div>
+
+          <input
+            className="btn btn-accent w-full"
+            value="Login"
+            type="submit"
+          />
+        </form>
+        <p>New to Doctors Portal <Link className="text-secondary" to='/signup'>Create New Account</Link></p>
+        <div className="divider">OR</div>
+        <button className="btn btn-outline w-full">CONTINUE WITH GOOGLE</button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
