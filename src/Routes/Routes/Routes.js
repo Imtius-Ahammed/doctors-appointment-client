a9b1b219a759f0b2e06210d3146df29df26bfb81
Imtius-Ahammed <ahammedimtius098@gmail.com>
2022-11-21 12:46:48 +0600

    create payment routes and install react stripe

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -7,10 +7,12 @@ import AllUsers from "../../Pages/Dashboard/AllUsers/AllUsers";
 import Dashboard from "../../Pages/Dashboard/Dashboard/Dashboard";
 import ManageDoctors from "../../Pages/Dashboard/ManageDoctors/ManageDoctors";
 import MyAppointment from "../../Pages/Dashboard/MyAppointment/MyAppointment";
+import Payment from "../../Pages/Dashboard/Payment/Payment";
 import Home from "../../Pages/Home/Home/Home";
 import Review from "../../Pages/Home/Testimonial/Review";
 import Login from "../../Pages/Login/Login";
 import SignUp from "../../Pages/SignUp/SignUp";
+import DisplayError from "../../Shared/DisplayError/DisplayError";
 import AdminRoute from "../AdminRoute/AdminRoute";
 import PrivateRoutes from "../PrivateRoutes/PrivateRoutes";
 
@@ -18,6 +20,7 @@ export const router = createBrowserRouter([
   {
     path:'/',
     element:<Main></Main>,
+    errorElement:<DisplayError></DisplayError>,
     children: [
       {
         path:'/',
@@ -46,6 +49,7 @@ export const router = createBrowserRouter([
   {
     path:'/dashboard',
     element:<PrivateRoutes><DashboardLayout></DashboardLayout></PrivateRoutes>,
+    errorElement:<DisplayError></DisplayError>,
     children:[
       {
         path:'/dashboard',
@@ -62,6 +66,11 @@ export const router = createBrowserRouter([
       {
         path:'/dashboard/managedoctors',
         element:<AdminRoute><ManageDoctors></ManageDoctors></AdminRoute>
+      },
+      {
+        path:'/dashboard/payment/:id',
+        element:<AdminRoute><Payment></Payment></AdminRoute>,
+        loader: ({params})=>fetch(`http://localhost:5000/bookings/${params.id}`)
       },
     ]
   }
